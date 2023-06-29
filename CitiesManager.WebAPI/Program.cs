5b4b0216263ee67290fe91a990917121a5aebbf3
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-06-29 20:49:17 +1200

    03. EntityFrameworkCore with Web API

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -1,8 +1,15 @@
+using CitiesManager.WebAPI.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers(); // This is because we are not going to create views in this project
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
