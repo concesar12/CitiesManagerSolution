16f6a8bf747d24740156db6251df3bcbb8ba6ec5
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-06-29 19:29:02 +1200

    02. Web API Controllers

---
+++ CitiesManager.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.WebAPI.Controllers
+{
+    //api is the literal name, it is a convention used
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController :ControllerBase
+    {
+        [HttpGet]
+        public string Method()
+        {
+            return "Hello World";
+        }
+    }
+}
