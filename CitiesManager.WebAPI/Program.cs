6666b537e5e8042546d6992658b729b5a2f8e2c6
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-07-01 14:41:41 +1200

    01. Swagger Open API

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -11,11 +11,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+//Swagger
+builder.Services.AddEndpointsApiExplorer(); // Generates description for all endpoints
+builder.Services.AddSwaggerGen(); //Geenerates OpenAPI specification
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseSwagger(); // creates endpoint for swagger.json
+app.UseSwaggerUI(); // creates swagger UI for testing all Web API endpoints / action methods
 
 app.UseAuthorization();
 
