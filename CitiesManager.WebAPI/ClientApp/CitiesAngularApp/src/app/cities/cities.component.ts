6b53c07330282386130ac20a344413298d448d31
Cesar David Guerrero Regino <concesar12@gmail.com>
2023-07-05 22:29:36 +1200

    put method

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { City } from '../models/city';
 import { CitiesService } from '../services/cities.service';
 
@@ -13,10 +13,21 @@ export class CitiesComponent {
   postCityForm: FormGroup;
   isPostCityFormSubmitted: boolean = false;
 
+  putCityForm: FormGroup;
+  editCityID: string | null = null;
+
   constructor(private citiesService: CitiesService) {
     this.postCityForm = new FormGroup({
       cityName: new FormControl(null, [Validators.required])
     });
+
+    this.putCityForm = new FormGroup({
+      cities: new FormArray([])
+    });
+  }
+
+  get putCityFormArray(): FormArray {
+    return this.putCityForm.get("cities") as FormArray;
   }
 
   loadCities() {
@@ -25,6 +36,13 @@ export class CitiesComponent {
 
         next: (response: City[]) => {
           this.cities = response;
+
+          this.cities.forEach(city => {
+            this.putCityFormArray.push(new FormGroup({
+              cityID: new FormControl(city.cityID, [Validators.required]),
+              cityName: new FormControl({ value: city.cityName, disabled: true }, [Validators.required]),
+            }));
+          });
         },
 
         error: (error: any) => {
@@ -67,6 +85,31 @@ export class CitiesComponent {
       },
       // eslint-disable-next-line @typescript-eslint/no-empty-function
       complete: () => { }
+    });
+  }
+
+  //Executes when the clicks on 'Edit' button the for the particular city
+  editClicked(city: City): void {
+    this.editCityID = city.cityID;
+  }
+
+  //executes when the clicks on 'Update' button after editing
+  updateClicked(i: number): void {
+
+    this.citiesService.putCity(this.putCityFormArray.controls[i].value).subscribe({
+      next: (response: string) => {
+        console.log(response);
+
+        this.editCityID = null;
+
+        this.putCityFormArray.controls[i].reset(this.putCityFormArray.controls[i].value);
+      },
+
+      error: (error: any) => {
+        console.log(error);
+      },
+
+      complete: () => { },
     });
   }
 }
